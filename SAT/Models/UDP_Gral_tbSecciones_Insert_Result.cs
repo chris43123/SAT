059ee1771289f15d0c4395636d96bf9cece2679a
EG0782 <53062171+EG0782@users.noreply.github.com>
2019-07-21 23:51:23 -0600

    EG_Secciones

--- SAT/Models/UDP_Gral_tbSecciones_Insert_Result.cs
+++ SAT/Models/UDP_Gral_tbSecciones_Insert_Result.cs
@@ -11,12 +11,8 @@ namespace SAT.Models
 {
     using System;
     
-    public partial class sp_helpdiagrams_Result
+    public partial class UDP_Gral_tbSecciones_Insert_Result
     {
-        public string Database { get; set; }
-        public string Name { get; set; }
-        public int ID { get; set; }
-        public string Owner { get; set; }
-        public int OwnerID { get; set; }
+        public string MensajeError { get; set; }
     }
 }
