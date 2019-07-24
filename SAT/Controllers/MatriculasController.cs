88ca18b1288b37cc252af6f658d55084023371f3
LAB01 <LAB01@DESKTOP-36LBO8D>
2019-07-24 14:47:39 -0600

    rectificacion de matriculas

--- SAT/Controllers/MatriculasController.cs
+++ SAT/Controllers/MatriculasController.cs
@@ -41,9 +41,9 @@ namespace SAT.Controllers
         {
             ViewBag.mat_UsuarioCrea = new SelectList(db.tbUsuarios, "usu_Id", "usu_NombreUsuario");
             ViewBag.mat_UsuarioModifica = new SelectList(db.tbUsuarios, "usu_Id", "usu_NombreUsuario");
-            ViewBag.alu_Id = new SelectList(db.tbAlumnos, "alu_Id", "alu_Identidad");
+            ViewBag.alu_Id = new SelectList(db.tbAlumnos, "alu_Id", "alu_Nombres");
             ViewBag.car_Id = new SelectList(db.tbCarreras, "car_Id", "car_Descripcion");
-            ViewBag.esc_Id = new SelectList(db.tbEscuelas, "esc_Id", "esc_Codigo");
+            ViewBag.esc_Id = new SelectList(db.tbEscuelas, "esc_Id", "esc_NombreEscuela");
             ViewBag.sec_Id = new SelectList(db.tbSecciones, "sec_Id", "sec_Descripcion");
             return View();
         }
@@ -55,37 +55,33 @@ namespace SAT.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "mat_Id,alu_Id,esc_Id,sec_Id,car_Id,mat_Anio,mat_UsuarioCrea,mat_FechaCrea,mat_UsuarioModifica,mat_FechaModifica")] tbMatriculas tbMatriculas)
         {
-            tbMatriculas.mat_Anio = DateTime.Now.Year;
-            tbMatriculas.mat_FechaCrea = DateTime.Now;
             tbMatriculas.mat_UsuarioCrea = 2;
+            tbMatriculas.mat_FechaCrea = DateTime.Now;
             if (ModelState.IsValid)
             {
                 try
                 {
-                    IEnumerable<object> ListMatricula = null;
+                    IEnumerable<object> listdepart = null;
                     string MensajeError = "";
+                    listdepart = db.UDP_Esc_tbMatriculas_Insert(tbMatriculas.alu_Id,
+                                                                    tbMatriculas.esc_Id,
+                                                                    tbMatriculas.sec_Id,
+                                                                    tbMatriculas.car_Id,
+                                                                    tbMatriculas.mat_Anio,                                                                                                                                        tbMatriculas.mat_UsuarioCrea,
+                                                                    tbMatriculas.mat_FechaCrea);
 
-                    ListMatricula = db.UDP_Esc_tbMatriculas_Insert(tbMatriculas.alu_Id,
-                                                                   tbMatriculas.esc_Id,
-                                                                   tbMatriculas.sec_Id,
-                                                                   tbMatriculas.car_Id,
-                                                                   tbMatriculas.mat_Anio,
-                                                                   tbMatriculas.mat_UsuarioCrea,
-                                                                   tbMatriculas.mat_FechaCrea
-                                                                   );
-                    foreach (UDP_Esc_tbMatriculas_Insert_Result res in ListMatricula)
+                    if (MensajeError.StartsWith("-1"))
                     {
-                        MensajeError = res.MensajeError;
-                    }
-
-                    if(MensajeError.StartsWith("-1"))
-                    {
-                        ModelState.AddModelError("", "1. No se pudo isertar el registro");
+                        ModelState.AddModelError("", "1. No se pudo insertar el registro.");
                         return View(tbMatriculas);
                     }
                     return RedirectToAction("Index");
+
+
+
+
                 }
-                catch(Exception Ex)
+                catch (Exception Ex)
                 {
                     Ex.Message.ToString();
                     ModelState.AddModelError("", "2. No se pudo insertar el registro.");
@@ -95,9 +91,9 @@ namespace SAT.Controllers
 
             ViewBag.mat_UsuarioCrea = new SelectList(db.tbUsuarios, "usu_Id", "usu_NombreUsuario", tbMatriculas.mat_UsuarioCrea);
             ViewBag.mat_UsuarioModifica = new SelectList(db.tbUsuarios, "usu_Id", "usu_NombreUsuario", tbMatriculas.mat_UsuarioModifica);
-            ViewBag.alu_Id = new SelectList(db.tbAlumnos, "alu_Id", "alu_Identidad", tbMatriculas.alu_Id);
+            ViewBag.alu_Id = new SelectList(db.tbAlumnos, "alu_Id", "alu_Nombres", tbMatriculas.alu_Id);
             ViewBag.car_Id = new SelectList(db.tbCarreras, "car_Id", "car_Descripcion", tbMatriculas.car_Id);
-            ViewBag.esc_Id = new SelectList(db.tbEscuelas, "esc_Id", "esc_Codigo", tbMatriculas.esc_Id);
+            ViewBag.esc_Id = new SelectList(db.tbEscuelas, "esc_Id", "esc_NombreEscuela", tbMatriculas.esc_Id);
             ViewBag.sec_Id = new SelectList(db.tbSecciones, "sec_Id", "sec_Descripcion", tbMatriculas.sec_Id);
             return View(tbMatriculas);
         }
@@ -116,9 +112,9 @@ namespace SAT.Controllers
             }
             ViewBag.mat_UsuarioCrea = new SelectList(db.tbUsuarios, "usu_Id", "usu_NombreUsuario", tbMatriculas.mat_UsuarioCrea);
             ViewBag.mat_UsuarioModifica = new SelectList(db.tbUsuarios, "usu_Id", "usu_NombreUsuario", tbMatriculas.mat_UsuarioModifica);
-            ViewBag.alu_Id = new SelectList(db.tbAlumnos, "alu_Id", "alu_Identidad", tbMatriculas.alu_Id);
+            ViewBag.alu_Id = new SelectList(db.tbAlumnos, "alu_Id", "alu_Nombres", tbMatriculas.alu_Id);
             ViewBag.car_Id = new SelectList(db.tbCarreras, "car_Id", "car_Descripcion", tbMatriculas.car_Id);
-            ViewBag.esc_Id = new SelectList(db.tbEscuelas, "esc_Id", "esc_Codigo", tbMatriculas.esc_Id);
+            ViewBag.esc_Id = new SelectList(db.tbEscuelas, "esc_Id", "esc_NombreEscuela", tbMatriculas.esc_Id);
             ViewBag.sec_Id = new SelectList(db.tbSecciones, "sec_Id", "sec_Descripcion", tbMatriculas.sec_Id);
             return View(tbMatriculas);
         }
@@ -130,39 +126,36 @@ namespace SAT.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "mat_Id,alu_Id,esc_Id,sec_Id,car_Id,mat_Anio,mat_UsuarioCrea,mat_FechaCrea,mat_UsuarioModifica,mat_FechaModifica")] tbMatriculas tbMatriculas)
         {
-            ViewBag.alu_Id = new SelectList(db.tbMatriculas, "alu_Id", "alu_Id");
-            tbMatriculas.mat_Anio = DateTime.Now.Year;
-            tbMatriculas.mat_FechaCrea = DateTime.Now;
-            tbMatriculas.mat_UsuarioCrea = 2;
+            tbMatriculas.mat_UsuarioModifica = 2;
+            tbMatriculas.mat_FechaModifica = DateTime.Now;
             if (ModelState.IsValid)
             {
+
                 try
                 {
-                    IEnumerable<object> ListMatricula = null;
+                    IEnumerable<object> listdepart = null;
                     string MensajeError = "";
-
-                    ListMatricula = db.UDP_Esc_tbMatriculas_Update(tbMatriculas.mat_Id,
-                                                                   tbMatriculas.esc_Id,
-                                                                   tbMatriculas.alu_Id,
-                                                                   tbMatriculas.sec_Id,
-                                                                   tbMatriculas.car_Id,
-                                                                   tbMatriculas.mat_Anio,
-                                                                   tbMatriculas.mat_UsuarioCrea,
-                                                                   tbMatriculas.mat_FechaCrea,
-                                                                   tbMatriculas.mat_UsuarioModifica,
-                                                                   tbMatriculas.mat_FechaModifica
-                                                                   );
-                    foreach (UDP_Esc_tbMatriculas_Update_Result res in ListMatricula)
-                    {
-                        MensajeError = res.MensajeError;
-                    }
+                    listdepart = db.UDP_Esc_tbMatriculas_Update(tbMatriculas.mat_Id,
+                                                                    tbMatriculas.alu_Id,
+                                                                    tbMatriculas.esc_Id,
+                                                                    tbMatriculas.sec_Id,
+                                                                    tbMatriculas.car_Id,
+                                                                    tbMatriculas.mat_Anio,
+                                                                    tbMatriculas.mat_UsuarioCrea,
+                                                                    tbMatriculas.mat_FechaCrea,
+                                                                    tbMatriculas.mat_UsuarioModifica,
+                                                                    tbMatriculas.mat_FechaModifica);
 
                     if (MensajeError.StartsWith("-1"))
                     {
-                        ModelState.AddModelError("", "1. No se pudo isertar el registro");
+                        ModelState.AddModelError("", "1. No se pudo insertar el registro.");
                         return View(tbMatriculas);
                     }
                     return RedirectToAction("Index");
+
+
+
+
                 }
                 catch (Exception Ex)
                 {
@@ -173,38 +166,14 @@ namespace SAT.Controllers
             }
             ViewBag.mat_UsuarioCrea = new SelectList(db.tbUsuarios, "usu_Id", "usu_NombreUsuario", tbMatriculas.mat_UsuarioCrea);
             ViewBag.mat_UsuarioModifica = new SelectList(db.tbUsuarios, "usu_Id", "usu_NombreUsuario", tbMatriculas.mat_UsuarioModifica);
-            ViewBag.alu_Id = new SelectList(db.tbAlumnos, "alu_Id", "alu_Identidad", tbMatriculas.alu_Id);
+            ViewBag.alu_Id = new SelectList(db.tbAlumnos, "alu_Id", "alu_Nombres", tbMatriculas.alu_Id);
             ViewBag.car_Id = new SelectList(db.tbCarreras, "car_Id", "car_Descripcion", tbMatriculas.car_Id);
-            ViewBag.esc_Id = new SelectList(db.tbEscuelas, "esc_Id", "esc_Codigo", tbMatriculas.esc_Id);
+            ViewBag.esc_Id = new SelectList(db.tbEscuelas, "esc_Id", "esc_NombreEscuela", tbMatriculas.esc_Id);
             ViewBag.sec_Id = new SelectList(db.tbSecciones, "sec_Id", "sec_Descripcion", tbMatriculas.sec_Id);
             return View(tbMatriculas);
         }
 
-        // GET: Matriculas/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            tbMatriculas tbMatriculas = db.tbMatriculas.Find(id);
-            if (tbMatriculas == null)
-            {
-                return HttpNotFound();
-            }
-            return View(tbMatriculas);
-        }
-
-        // POST: Matriculas/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            tbMatriculas tbMatriculas = db.tbMatriculas.Find(id);
-            db.tbMatriculas.Remove(tbMatriculas);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
+ 
 
         protected override void Dispose(bool disposing)
         {
